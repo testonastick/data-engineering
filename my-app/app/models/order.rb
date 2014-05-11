2581227818d45dbe3ff4eefead72b0dff0900556
testonastick <testonastick@gmail.com>
2014-05-11 15:38:45 -0500

    Rounded out merchant validation
    - test
    - model change

--- my-app/app/models/order.rb
+++ my-app/app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
   belongs_to :merchant
-  attr_accessible :item_description, :item_price, :purchase_count, :purchaser_name, :uploaded_at
+  attr_accessible :item_description, :item_price, :purchase_count, :purchaser_name, :uploaded_at, :merchant
   validates :merchant, presence: true
 end
--- my-app/spec/models/order_spec.rb
+++ my-app/spec/models/order_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 describe Order do
   it "is not valid without a merchant" do
-    #merchant = Merchant.new :address => '1122 boogie boogie ave', :name => 'booshop'
     order = Order.new
     expect(order).to_not be_valid
+  end
+  it "is valid with a merchant" do
+    merchant = Merchant.new :address => '1122 boogie boogie ave', :name => 'booshop'
+    merchant.save
+    order = Order.new :merchant => merchant
+    expect(order).to be_valid
   end
 end
